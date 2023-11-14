8d2f5bfded13e9955b105a9b4c0b7c6cc737d9f5
Eyuep Sueyruege <eyuep.sueyruege@noreply.ais-lab.eu>
2023-11-14 16:16:08 +0100

    Update src/client/main.js

--- src/client/main.js
+++ src/client/main.js
@@ -1,1 +1,16 @@
-//
+// Starting the game
+
+function startGame(){
+
+    let mainfield = $("#mainfield");
+
+    mainfield.html('<div class="container">    <div class="row">        <div class="col-md-2"></div>        <div class="col-md-8">            <div class="player-box">Player 1</div>        </div>        <div class="col-md-2"></div>    </div>    <div class="row">        <div class="col-md-2">            <div class="player-box">Player 4</div>        </div>        <div class="col-md-8">            <div class="container">                <div class="row">                    <div class="col-md-6">                        <div class="player-box">Mitte</div>                    </div>                    <div class="col-md-6">                        <div class="player-box">Dealer</div>                    </div>                </div>            </div>        </div>        <div class="col-md-2">            <div class="player-box">Player 2</div>        </div>    </div>    <div class="row">        <div class="col-md-2"></div>        <div class="col-md-8">            <div class="player-box">Player 3</div>        </div>        <div class="col-md-2"></div>    </div></div>');
+
+
+}
+
+function refresh(){
+
+    
+
+}
